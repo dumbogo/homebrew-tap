dc569c72041b5d3542787443d51a278046a8c513
Luis Aguilera <aguilera.glz.luis@gmail.com>
2021-06-16 23:05:28 -0500

    Add unneded bottle chess Formula

--- chess.rb
+++ chess.rb
@@ -10,15 +10,18 @@ class Chess < Formula
 
   # depends_on "cmake" => :build
 
+
+  # TODO: Hardcoding to MacOSX, implement more when needed
+  url "https://github.com/dumbogo/chess/releases/download/v1.0.0/chess-v2.0.0-darwin-amd64.tar.gz"
+  sha256 "ac4647615348e2eede6c5ad5de159bb9f57d1bbf54692e52a45b687afae6744c"
+
+  bottle :unneeded
+
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     bin.install "chess"
   end
-
-  # TODO: Hardcoding to MacOSX, implement more when needed
-  url "https://github.com/dumbogo/chess/releases/download/v1.0.0/chess-v2.0.0-darwin-amd64.tar.gz"
-  sha256 "ac4647615348e2eede6c5ad5de159bb9f57d1bbf54692e52a45b687afae6744c"
 
   # test do
   #   # `test do` will create, run in and delete a temporary directory.
