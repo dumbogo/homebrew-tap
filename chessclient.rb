5251247bdc083d9860271150feae9e1d02d7834f
Luis Aguilera <aguilera.glz.luis@gmail.com>
2021-06-16 22:45:08 -0500

    update chess formula

--- chessclient.rb
+++ chessclient.rb
@@ -4,9 +4,8 @@
 class DumbogoChess < Formula
   desc ""
   homepage "github.com/dumbogo/chess"
-  url "dchess"
+  url "github.com/dumbogo/chess"
   version "1.0.0"
-  sha256 ""
   license "MIT"
 
   # depends_on "cmake" => :build
@@ -14,23 +13,23 @@ class DumbogoChess < Formula
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+    bin.install "chess"
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test dumbogo-chess`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
+  # TODO: Hardcoding to MacOSX, implement more when needed
+  url "https://github.com/dumbogo/chess/releases/download/v1.0.0/chess-v2.0.0-darwin-amd64.tar.gz"
+  sha256 "ac4647615348e2eede6c5ad5de159bb9f57d1bbf54692e52a45b687afae6744c"
+
+  # test do
+  #   # `test do` will create, run in and delete a temporary directory.
+  #   #
+  #   # This test will fail and we won't accept that! For Homebrew/homebrew-core
+  #   # this will need to be a test that verifies the functionality of the
+  #   # software. Run the test with `brew test dumbogo-chess`. Options passed
+  #   # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+  #   #
+  #   # The installed folder is not in the path, so use the entire path to any
+  #   # executables being tested: `system "#{bin}/program", "do", "something"`.
+  #   system "false"
+  # end
 end
